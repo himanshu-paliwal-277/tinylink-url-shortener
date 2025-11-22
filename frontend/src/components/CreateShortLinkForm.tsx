50a737d2032b039eadd48d6c4d146db033b39451
Himanshu Paliwal <himanshupaliwal.inventurs@gmail.com>
2025-11-22 21:48:13 +0530

    fix build issue and format the code

--- frontend/src/components/CreateShortLinkForm.tsx
+++ frontend/src/components/CreateShortLinkForm.tsx
@@ -3,7 +3,7 @@
 import React, { memo, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
-import { Card, CardContent,CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useCreateLink } from '@/hooks/useCreateLink';
